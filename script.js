230d8234870e346d4e2f6e4640b5beb8f9100cb1
Jedrzej Dudzicz <jedrzejdudzicz@gmail.com>
2022-11-28 00:54:55 +0100

    Dodawnie nowych porduktów

---
+++ script.js
@@ -0,0 +1,12 @@
+const buttonSubmit = () => {
+    let nameInput = document.getElementById("name").value;
+    let quantityInput = document.getElementById("quantity").value;
+    let priceInput = document.getElementById("price").value;
+    
+    const table = document.getElementById("products");
+    let template = `<tr> <td></td> <td>${nameInput}</td> <td>${quantityInput}</td> <td>${priceInput}zł</td> <td>${quantityInput*priceInput}zł</td> </tr> `;    
+    table.innerHTML += template;
+    rows = table.getElementsByTagName('tr');
+    console.log(rows.length);
+    rows[rows.length-1].children[0]["innerText"] = rows.length-1;
+}
