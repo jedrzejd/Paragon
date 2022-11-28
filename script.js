260c071e6b540833e04127cb31e90ad59db18ad1
Jedrzej Dudzicz <jedrzejdudzicz@gmail.com>
2022-11-28 21:15:06 +0100

    poprawienie dodawania produktu

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const buttonSubmit = () => {
     let nameInput = document.getElementById("name").value;
     let quantityInput = document.getElementById("quantity").value;
     let priceInput = document.getElementById("price").value;
-    
-    const table = document.getElementById("products");
+
+    const table = document.getElementById("tbody-products");
     let template = `<tr> <td></td> <td>${nameInput}</td> <td>${quantityInput}</td> <td>${priceInput}zł</td> <td>${quantityInput*priceInput}zł</td> </tr> `;    
+ 
     table.innerHTML += template;
     rows = table.getElementsByTagName('tr');
     console.log(rows.length);
     rows[rows.length-1].children[0]["innerText"] = rows.length-1;
+}
+
+const deleteRow = (number) => {
+    document.getElementById("row"+number+"").outerHTML="";
 }
