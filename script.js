fdddffcc9cc84f622b500050ca90f8f7a0f8a776
xawian <66998201+xawian@users.noreply.github.com>
2022-11-29 12:47:56 +0100

    Add move

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+$('.table-up').click(function () {
+    var $row = $(this).parents('tr');
+    if ($row.index() === 1) return; // Don't go above the header
+    $row.prev().before($row.get(0));
+    console.log("gora")
+  });
+
+
+$('.table-down').click(function () {
+    var $row = $(this).parents('tr');
+    $row.next().after($row.get(0));
+    console.log("dol")
+  });
+
+
 const buttonSubmit = () => {
     let nameInput = document.getElementById("name").value;
     let quantityInput = document.getElementById("quantity").value;
@@ -19,12 +35,17 @@ const buttonSubmit = () => {
     let table = document.getElementById("tbody-products");
     let rows = table.getElementsByTagName('tr');
 
-    let template = `<tr> <td></td> <td>${nameInput}</td> <td>${quantityInput}</td> <td>${priceInput}zł</td> <td>${quantityInput * priceInput}zł</td> <td> <input type="submit" value="Usuń" class="btn btn-danger" id="Delete_button" onclick="deleteRow(${rows.length})"></td></tr> `;
+    let template = `<tr> <td></td> <td>${nameInput}</td> <td>${quantityInput}</td> <td>${priceInput}zł</td> <td>${quantityInput * priceInput}zł</td> <td><svg class="table-up" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+  </svg>
+  <svg class="table-down" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
+  </svg> </td><td> <input type="submit" value="Usuń" class="btn btn-danger" id="Delete_button" onclick="deleteRow(${rows.length})"></td></tr> `;
 
     table.innerHTML += template;
 
     // console.log(rows.length);
-    rows[rows.length - 1].children[0]["innerText"] = rows.length;
+    rows[rows.length - 1].children[0]["innerText"] = rows.length; //trzeba to pozmieniac, miejsce tbody zostalo zmienione
     rows[rows.length - 1].onclick = function () {
         rIndex = this.rowIndex;
         console.log(rIndex);
@@ -49,7 +70,6 @@ const editNumberRow = () => {
 
     for (var i = 0; i < table.rows.length; i++) {
         table.rows[i].cells[0].innerHTML = i + 1;
-        table.rows[i].cells[5].innerHTML = `<input type="submit" value="Usuń" id="Delete_button" class="btn btn-danger" onclick="deleteRow(${i})"></input>`;
     }
 }
 
